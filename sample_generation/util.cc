dc27566de15cd822ed28c97979c01d25184a5023
Andrew-Helmer <andrew@andrewhelmer.com>
2020-05-30 01:21:32 -0400

    Update best-candidate sampling to check distances in toroidal space.

--- sample_generation/util.cc
+++ sample_generation/util.cc
@@ -43,14 +43,25 @@ int UniformInt(int min, int max) {
 }
 
 namespace {
-inline double DistSq(double x1, double y1, double x2, double y2) {
-  double x_diff = x2-x1, y_diff = y2-y1;
+inline double DistSqTorus(double x1, double y1, double x2, double y2) {
+  double x_diff = abs(x2-x1);
+  if (x_diff > 0.5) x_diff = 1.0 - x_diff;
+  double y_diff = abs(y2-y1);
+  if (y_diff > 0.5) y_diff = 1.0 - y_diff;
+
   return (x_diff*x_diff)+(y_diff*y_diff);
 }
 
-double GetNearestNeighborDistSq(const Point& sample,
-                                const Point* sample_grid[],
-                                const int dim) {
+int Wrap(const int val,
+         const int dim) {
+  if (val < 0) return val+dim;
+  if (val >= dim) return val-dim;
+  return val;
+}
+
+double GetNearestNeighborDistSqTorus(const Point& sample,
+                                     const Point* sample_grid[],
+                                     const int dim) {
   // This function works by using the sample grid, since we know that the points
   // are well-distributed with at most one point in each cell. Not the fastest
   // way to do this, but easy to implement and not horribly slow.
@@ -70,35 +81,48 @@ double GetNearestNeighborDistSq(const Point& sample,
     double grid_radius = grid_size * (i + 0.7072);
     double grid_radius_sq = grid_radius * grid_radius;
 
-    const int x_min = std::max(x_pos - i, 0);
-    const int x_max = std::min(x_pos + i, dim-1);
-    const int y_min = std::max(y_pos - i, 0);
-    const int y_max = std::min(y_pos + i, dim-1);
+    const int x_min = x_pos - i;
+    const int x_max = x_pos + i;
+    const int y_min = y_pos - i;
+    const int y_max = y_pos + i;
     // Traverse top and bottom boundaries, including corners.
     for (int x_offset = x_min; x_offset <= x_max; x_offset++) {
-      const Point* top_pt = sample_grid[y_max*dim+x_offset];
-      const Point* bottom_pt = sample_grid[y_min*dim+x_offset];
-      if (top_pt != nullptr)
-          min_dist_sq = std::min(min_dist_sq,
-                                 DistSq(top_pt->x, top_pt->y,
-                                        sample.x, sample.y));
-      if (bottom_pt != nullptr)
-          min_dist_sq = std::min(min_dist_sq,
-                                 DistSq(bottom_pt->x, bottom_pt->y,
-                                        sample.x, sample.y));
+      int wrapped_x_offset = Wrap(x_offset, dim);
+      int wrapped_y_min = Wrap(y_min, dim);
+      int wrapped_y_max = Wrap(y_max, dim);
+
+      const Point* top_pt = sample_grid[wrapped_y_max*dim + wrapped_x_offset];
+      const Point* bottom_pt =
+          sample_grid[wrapped_y_min*dim + wrapped_x_offset];
+      if (top_pt != nullptr) {
+        min_dist_sq = std::min(min_dist_sq,
+                               DistSqTorus(top_pt->x, top_pt->y,
+                                           sample.x, sample.y));
+      }
+      if (bottom_pt != nullptr) {
+        min_dist_sq = std::min(min_dist_sq,
+                               DistSqTorus(bottom_pt->x, bottom_pt->y,
+                                           sample.x, sample.y));
+      }
     }
     // Traverse left and right sides, excluding corners (hence the +1, -1).
     for (int y_offset = y_min+1; y_offset <= y_max-1; y_offset++) {
-      const Point* left_pt = sample_grid[y_offset*dim+x_min];
-      const Point* right_pt = sample_grid[y_offset*dim+x_max];
-      if (left_pt != nullptr)
-          min_dist_sq = std::min(min_dist_sq,
-                                 DistSq(left_pt->x, left_pt->y,
-                                        sample.x, sample.y));
-      if (right_pt != nullptr)
-          min_dist_sq = std::min(min_dist_sq,
-                                 DistSq(right_pt->x, right_pt->y,
-                                        sample.x, sample.y));
+      int wrapped_y_offset = Wrap(y_offset, dim);
+      int wrapped_x_min = Wrap(x_min, dim);
+      int wrapped_x_max = Wrap(x_max, dim);
+
+      const Point* left_pt = sample_grid[wrapped_y_offset*dim + wrapped_x_min];
+      const Point* right_pt = sample_grid[wrapped_y_offset*dim + wrapped_x_max];
+      if (left_pt != nullptr) {
+        min_dist_sq = std::min(min_dist_sq,
+                               DistSqTorus(left_pt->x, left_pt->y,
+                                           sample.x, sample.y));
+      }
+      if (right_pt != nullptr) {
+        min_dist_sq = std::min(min_dist_sq,
+                               DistSqTorus(right_pt->x, right_pt->y,
+                                           sample.x, sample.y));
+      }
     }
 
     if (min_dist_sq < grid_radius_sq) {
@@ -122,7 +146,7 @@ Point GetBestCandidateOfSamples(const std::vector<Point>& candidates,
   for (int i = 0; i < candidates.size(); i++) {
     Point cand_sample = candidates[i];
     double dist_sq =
-        GetNearestNeighborDistSq(cand_sample, sample_grid, dim);
+        GetNearestNeighborDistSqTorus(cand_sample, sample_grid, dim);
     if (dist_sq > max_dist_sq) {
       best_candidate = cand_sample;
       max_dist_sq = dist_sq;
